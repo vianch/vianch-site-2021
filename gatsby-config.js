3a781272ed02272f99fe75868289b0dcadf27de9
Victor <vachavarro@gmail.com>
2021-04-09 18:38:06 -0500

    :chart_with_upwards_trend: (analytics) add google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,23 @@ module.exports = {
       resolve: "gatsby-plugin-sass",
     },
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-15967013-3", // Google Analytics / GA
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5X6DTRX",
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
   ],
   siteMetadata: {
     title: "VIANCH.com",
